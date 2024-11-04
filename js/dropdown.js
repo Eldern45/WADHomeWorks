d6d33da6f8dfff09319867796452c90234cb82cc
Johan Veenpere <johan.veenpere@gmail.com>
2024-11-04 22:20:21 +0200

    changed dropdown to close when clicked elsewhere on page

--- js/dropdown.js
+++ js/dropdown.js
@@ -5,12 +5,21 @@ let name = document.createElement("p")
 let email = document.createElement("p")
 let logout = document.createElement("p")
 name.innerText = "Homer Simpson"
-email.innerHTML = "donuts4life@springmail.com"
+name.id = "dontclose"
+email.innerHTML = "donuts4life@springmail.com";
+email.id = "dontclose"
 logout.innerHTML = "Logout"
+logout.id = "dontclose"
 dropdown.appendChild(name)
 dropdown.appendChild(email)
 dropdown.appendChild(logout)
 
+document.onclick = e => {
+    console.log(e.target)
+    if (e.target.id !== "profile" && e.target.id !== "dontclose") {
+        dropdown.style.visibility = "hidden"
+    }
+}
 
 profile.onclick = e => {
     if (dropdown.style.visibility === "visible") {
@@ -23,6 +32,7 @@ profile.onclick = e => {
 
 document.body.appendChild(dropdown)
 dropdown.classList.add("dropdown")
+dropdown.id = "dontclose"
 dropdown.style.visibility = "hidden"
 dropdown.style.position = "absolute"
 dropdown.style.right = "5px"
