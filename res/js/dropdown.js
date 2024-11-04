ad8bc6cff63b032566bd311e446080e8a064b5bb
Johan Veenpere <johan.veenpere@gmail.com>
2024-11-04 16:50:43 +0200

    Changed some hardcoded values

--- res/js/dropdown.js
+++ res/js/dropdown.js
@@ -4,8 +4,8 @@ let dropdown = document.createElement("div")
 let name = document.createElement("p")
 let email = document.createElement("p")
 let logout = document.createElement("p")
-name.innerText = "Dumb User"
-email.innerHTML = "dumbuser@berkeley.edu"
+name.innerText = "Homer Simpson"
+email.innerHTML = "donuts4life@springmail.com"
 logout.innerHTML = "Logout"
 dropdown.appendChild(name)
 dropdown.appendChild(email)
@@ -26,4 +26,4 @@ dropdown.classList.add("dropdown")
 dropdown.style.visibility = "hidden"
 dropdown.style.position = "fixed"
 dropdown.style.right = "5px"
-dropdown.style.top = "70px"
+dropdown.style.top = "76px"
